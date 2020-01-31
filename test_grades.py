f94c47cdfd28f40149a60a88d44ea26b0fd12b0e
Ben Coleman <colemanb@moravian.edu>
2020-01-31 14:48:37 -0500

    Add test for equal grades

--- test_grades.py
+++ test_grades.py
@@ -15,3 +15,8 @@ def test_single_grade():
 def test_two_grades():
     grades = [10, 30]
     assert compute_hw_average(grades) == 20
+
+
+def test_two_equal_grades():
+    grades = [30, 30]
+    assert compute_hw_average(grades) == 30
